1727a53359d092b90d418f4f86d5f3940e5d9353
HP <HP@LAPTOP-SMC8HIAI>
2022-04-11 14:46:00 +0530

    my first commit

---
+++ qspautomationproject/src/qsp/LaunchChromeBrowser.java
@@ -0,0 +1,14 @@
+package qsp;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChromeBrowser {
+
+	public static void main(String[] args) {
+		
+		
+		new ChromeDriver(); //open the chrome browser
+		
+	}
+
+}
