7a924c398edc96414879ca19956f563c7bab1920
HP <HP@LAPTOP-SMC8HIAI>
2022-04-13 14:00:10 +0530

    hhh

--- qspautomationproject/src/qsp/LaunchChromeBrowser.java
+++ qspautomationproject/src/qsp/LaunchChromeBrowser.java
@@ -7,7 +7,7 @@ public class LaunchChromeBrowser {
 	public static void main(String[] args) {
 		
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
-		new ChromeDriver(); //open the chrome browser
+		new ChromeDriver(); //open the chrome browser1
 		
 	}
 
