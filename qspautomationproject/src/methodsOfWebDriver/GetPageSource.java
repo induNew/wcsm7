fd6e4b5ba61adb17b5c3339ef284896bb6500d71
HP <HP@LAPTOP-SMC8HIAI>
2022-04-16 09:22:36 +0530

    new c123

--- qspautomationproject/src/methodsOfWebDriver/GetPageSource.java
+++ qspautomationproject/src/methodsOfWebDriver/GetPageSource.java
@@ -3,15 +3,16 @@ package methodsOfWebDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GetCurrentUrl {
-	
-	
+public class GetPageSource {
+
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.selenium.dev");
-		String currentUrl = driver.getCurrentUrl();
-		System.out.println(currentUrl);
+		driver.manage().window().maximize();
+		driver.get("file:///C:/Users/HP/Desktop/1wcsm7htmldocs/checkbox.html");
+		String sourceCodeOfTheWebPage = driver.getPageSource();
+		
+		System.out.println(sourceCodeOfTheWebPage);
 	}
 
 }
