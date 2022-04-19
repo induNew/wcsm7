0d1fea36dd458b7825f931ba1190c79cec406a9b
HP <HP@LAPTOP-SMC8HIAI>
2022-04-19 17:06:03 +0530

    aswaw

--- qspautomationproject/src/locators/IdLocator.java
+++ qspautomationproject/src/locators/IdLocator.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class IdLocator {
-
+//
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
