bf1ce4dd5a2fa6a8f389721e4431fdda809f08eb
HP <HP@LAPTOP-SMC8HIAI>
2022-04-28 16:11:42 +0530

    s

--- qspautomationproject/src/locators/NameLocator.java
+++ qspautomationproject/src/locators/NameLocator.java
@@ -11,7 +11,7 @@ public class NameLocator {
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		
-		//login operation
+		//login operatio
 		
 		driver.findElement(By.name("txtUsername")).sendKeys("Admin");//type username
 		driver.findElement(By.name("txtPassword")).sendKeys("admin123");//type password
