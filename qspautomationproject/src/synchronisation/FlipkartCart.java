31b386a16456812ba59280b4d6707cf76a6e2ecf
HP <HP@LAPTOP-SMC8HIAI>
2022-05-05 11:14:49 +0530

    nc

--- qspautomationproject/src/synchronisation/FlipkartCart.java
+++ qspautomationproject/src/synchronisation/FlipkartCart.java
@@ -6,27 +6,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class InstagramLogin {
+public class FlipkartCart {
+
 	
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
-		driver.get("https://www.instagram.com");
+		
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.get("https://www.flipkart.com/");
 		
 		
+		driver.findElement(By.xpath("//button[.='✕']")).click();
 		
-		driver.findElement(By.name("username")).sendKeys("surajsawant");
-		driver.findElement(By.name("password")).sendKeys("idontknow");
-		driver.findElement(By.cssSelector("button[class*='L3NKy']")).click();
+		driver.findElement(By.xpath("//span[.='Cart']")).click();
 		
-		
-		
-		
-		
-		
+		driver.findElement(By.xpath("//span[.='Login123']")).click();
 		
 	}
-
 }
